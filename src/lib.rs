741b83962bae44260d752864cd667065c70ea273
bardunbar <61152068+bardunbar@users.noreply.github.com>
2023-08-26 15:19:21 +0100

    Textures and bind groups + challenge

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use winit::{
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+mod texture;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
 struct Vertex {
@@ -52,7 +54,7 @@ const VERTICES: &[Vertex] = &[
     Vertex { position: [0.35966998, -0.3473291, 0.0], color: [1.0, 1.0, 1.0], tex_coords: [0.85967, 0.84732914], }, // D
     Vertex { position: [0.44147372, 0.2347359, 0.0], color: [1.0, 1.0, 1.0], tex_coords: [0.9414737, 0.2652641], }, // E
 ];
- 
+
 
 const INDICES: &[u16] = &[
     0, 1, 4,
@@ -74,9 +76,13 @@ struct State {
     index_buffer: wgpu::Buffer,
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
+    diffuse_texture: texture::Texture,
+    alternate_bind_group: wgpu::BindGroup,
+    alternate_texture: texture::Texture,
 
     // Custom variables
     clear_color: wgpu::Color,
+    texture_toggle_state: bool,
 }
 
 impl State {
@@ -133,67 +139,22 @@ impl State {
         surface.configure(&device, &config);
 
         let diffuse_bytes = include_bytes!("happy-tree.png");
-        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-        let diffuse_rgba = diffuse_image.to_rgba8();
+        let diffuse_texture = texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree-texture").unwrap();
 
-        use image::GenericImageView;
-        let dimensions = diffuse_image.dimensions();
+        let alternate_texture_bytes = include_bytes!("other-tree.png");
+        let alternate_texture = texture::Texture::from_bytes(&device, &queue, alternate_texture_bytes, "other-tree-texture").unwrap();
 
-        let texture_size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1,
-        };
-        let diffuse_texture = device.create_texture(
-            &wgpu::TextureDescriptor {
-                label: Some("diffuse_texture"),
-                size: texture_size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
-                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-                view_formats: &[],
-            }
-        );
 
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &diffuse_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            }, 
-            &diffuse_rgba, 
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
-            }, 
-            texture_size,
-        );
-
-        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { 
-            label: Some("texture_bind_group_layout"), 
+        let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("texture_bind_group_layout"),
             entries: &[
                 wgpu::BindGroupLayoutEntry {
                     binding: 0,
                     visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Texture { 
-                        sample_type: wgpu::TextureSampleType::Float { filterable: true }, 
-                        view_dimension: wgpu::TextureViewDimension::D2, 
-                        multisampled: false 
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false
                     },
                     count: None,
                 },
@@ -211,14 +172,31 @@ impl State {
                 entries: &[
                     wgpu::BindGroupEntry {
                         binding: 0,
-                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                        resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
                     },
                     wgpu::BindGroupEntry {
                         binding: 1,
-                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler)
+                        resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler)
                     },
                 ],
                 label: Some("diffuse_bind_group"),
+            }
+        );
+
+        let alternate_bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&alternate_texture.view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&alternate_texture.sampler)
+                    },
+                ],
+                label: Some("alternate_bind_group"),
             }
         );
 
@@ -305,9 +283,13 @@ impl State {
             index_buffer,
             num_indices,
             diffuse_bind_group,
+            diffuse_texture,
+            alternate_bind_group,
+            alternate_texture,
 
             // Custom Variables
-            clear_color: wgpu::Color { r: 0.1, g: 0.2, b: 0.0, a: 1.0, }
+            clear_color: wgpu::Color { r: 0.1, g: 0.2, b: 0.0, a: 1.0, },
+            texture_toggle_state: false
         }
     }
 
@@ -330,6 +312,16 @@ impl State {
             WindowEvent::CursorMoved { position, .. } => {
                 self.clear_color.r = position.x / self.size.width as f64;
                 self.clear_color.g = position.y / self.size.height as f64;
+            },
+            WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Space),
+                    ..
+                },
+                ..
+            } => {
+                self.texture_toggle_state = !self.texture_toggle_state;
             },
             _ => {}
         }
@@ -364,7 +356,16 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+
+            {
+                let bind_group = if self.texture_toggle_state {
+                    &self.alternate_bind_group
+                } else {
+                    &self.diffuse_bind_group
+                };
+                render_pass.set_bind_group(0, bind_group, &[]);
+            }
+
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
