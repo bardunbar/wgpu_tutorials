efb10477f30efc92544882ba0e560ee77afab09e
bardunbar <61152068+bardunbar@users.noreply.github.com>
2023-08-31 21:52:34 +0100

    Pixel Depth Buffer

--- src/lib.rs
+++ src/lib.rs
@@ -92,22 +92,22 @@ impl InstanceRaw {
             step_mode: wgpu::VertexStepMode::Instance,
             attributes: &[
                 wgpu::VertexAttribute {
-                    offset: 0, 
+                    offset: 0,
                     shader_location: 5,
                     format: wgpu::VertexFormat::Float32x4,
                 },
                 wgpu::VertexAttribute {
-                    offset: mem::size_of::<[f32;4]>() as wgpu::BufferAddress, 
+                    offset: mem::size_of::<[f32;4]>() as wgpu::BufferAddress,
                     shader_location: 6,
                     format: wgpu::VertexFormat::Float32x4,
                 },
                 wgpu::VertexAttribute {
-                    offset: mem::size_of::<[f32;8]>() as wgpu::BufferAddress, 
+                    offset: mem::size_of::<[f32;8]>() as wgpu::BufferAddress,
                     shader_location: 7,
                     format: wgpu::VertexFormat::Float32x4,
                 },
                 wgpu::VertexAttribute {
-                    offset: mem::size_of::<[f32;12]>() as wgpu::BufferAddress, 
+                    offset: mem::size_of::<[f32;12]>() as wgpu::BufferAddress,
                     shader_location: 8,
                     format: wgpu::VertexFormat::Float32x4,
                 },
@@ -172,6 +172,7 @@ struct State {
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
     num_indices: u32,
+    depth_texture: texture::Texture,
     diffuse_bind_group: wgpu::BindGroup,
     diffuse_texture: texture::Texture,
     alternate_bind_group: wgpu::BindGroup,
@@ -242,6 +243,8 @@ impl State {
         };
 
         surface.configure(&device, &config);
+
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
 
         let diffuse_bytes = include_bytes!("happy-tree.png");
         let diffuse_texture = texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree-texture").unwrap();
@@ -400,7 +403,13 @@ impl State {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -460,6 +469,7 @@ impl State {
             vertex_buffer,
             index_buffer,
             num_indices,
+            depth_texture,
             diffuse_bind_group,
             diffuse_texture,
             alternate_bind_group,
@@ -488,6 +498,8 @@ impl State {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+
+            self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -556,7 +568,14 @@ impl State {
                         store: true
                     }
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
+                }),
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
