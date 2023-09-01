f9ea1f5c4de3d4a55b8ce57a4450bd9b908c2d65
bardunbar <ian@dunbargames.com>
2023-09-01 13:46:09 +0100

    Model Loading Part 2

--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,23 @@
 
 use crate::texture;
+use core::ops::Range;
+
+pub trait DrawModel<'a> {
+    fn draw_mesh(&mut self, mesh: &'a Mesh);
+    fn draw_mesh_instanced(&mut self, mesh: &'a Mesh, instances: Range<u32>);
+}
+
+impl<'a, 'b> DrawModel<'b> for wgpu::RenderPass<'a> where 'b: 'a, {
+    fn draw_mesh(&mut self, mesh: &'b Mesh) {
+        self.draw_mesh_instanced(mesh, 0..1);
+    }
+
+    fn draw_mesh_instanced(&mut self, mesh: &'b Mesh, instances: Range<u32>) {
+        self.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
+        self.set_index_buffer(mesh.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+        self.draw_indexed(0..mesh.num_elements, 0, instances);
+    }
+}
 
 pub struct Model {
     pub meshes: Vec<Mesh>,
@@ -59,4 +77,3 @@ impl Vertex for ModelVertex {
         }
     }
 }
-
